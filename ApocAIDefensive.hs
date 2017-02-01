c6614afa9fe13af977accdc5208fa18787cfb5a4
Tit-tytant <saurabh.tomar@gmail.com>
2017-02-01 12:52:22 -0700

    Update Defensive AI

--- ApocAIDefensive.hs
+++ ApocAIDefensive.hs
@@ -4,13 +4,18 @@
 -- The Defensive AI will scan the board from friendly to enemy territory. The friendly unit 
 -- that first encounters an enemy unit in it's strike position will move in for the kill.
 
--- The pawn will check 2 positions for victims
+-- The pawn will check 2 positions for victims.
+--  For White units (moving top to bottom):
 --    (currX-1,currY+1), 
---    (currX+1,currY+1)
+--    (currX+1,currY+1),
+--  For Black units (moving bottom to top):
+--    (currX-1,currY-1), 
+--    (currX+1,currY-1),
 
 -- The knight will check 8 positions 
 --    (currX-2,currY-1), (currX-2,currY+1), 
 --    (currX+2,currY-1), (currX+2,currY+1), 
 --    (currX-1, currY-2), (currX+1,currY-2),
 --    (currX+2,currY-1), (currX+2,currY+1)
+
 -- If the move is valid and there is a target then the AI will take that move
