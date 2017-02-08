8b75542eafb9aad88e6919c4a0762cb819937f80
TriickShotz <matthew@mullinsfamily.ca>
2017-02-08 12:54:24 -0700

    Update Offensive.hs

--- AI/Offensive.hs
+++ AI/Offensive.hs
@@ -6,3 +6,26 @@ import Lib.Functions
 
 aiOffensive :: Chooser
 aiOffensive currBoard Normal playerType = return (Just [(2,0),(2,1)])
+
+--Check playerType
+  --if BLACK
+    --if (BK == 0)
+      --if possiblePawnCapture
+        --select random possiblePawnCapture
+        --RETURN
+      --else no possiblePawnCapture
+        --select random WP movement
+        --RETURN
+    --else (BK != 0)
+      --if possibleKnightCapture
+        --select random possibleKnightCapture
+        --RETURN
+      --else no possibleKnightCapture
+        --if possiblePawnCapture
+          --select random possiblePawnCapture
+          --RETURN
+        --else no possiblePawnCapture
+          --select random WP movement
+          --RETURN
+          
+  --else WHITE
