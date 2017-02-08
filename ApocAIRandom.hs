f06f4142dd8d474aea6efc4b05b262f7d6f6848a
kstelter <kaylee.stelter@ucalgary.ca>
2017-02-07 18:08:19 -0700

    Additions to RandomAI
    Does not compile

--- ApocAIRandom.hs
+++ ApocAIRandom.hs
@@ -4,26 +4,39 @@
 
 
 
-module ApocAIRandom 
-       (main, 
-       createMove) where 
+module ApocAIRandom
+       (main,
+       createMove) where
 
-import System.Random 
+import System.Random
 import Data.Char
 import ApocTools
 
--- Randomly chooses one of those pieces to move
+-- List of coordinates representing the game board
+coordinateBoard =  [ [(0,0), (0,1), (0,2), (0,3), (0,4)],
+                    [(1,0), (1,1) , (1,2) , (1,3) , (1,4)],
+                    [(2,0), (2,1) , (2,2) , (2,3) , (2,4) ],
+                    [(3,0), (3,1) , (3,2) , (3,3) , (3,4)],
+                    [(4,0), (4,1), (4,2), (4,3), (4,4)] ]
+
+
 -- Randomly chooses from a legal move, pass, or goof
 
--- Should also handle the pawn placement?
- 
+-- Should also handle the pawn placement
+
 -- Generates two integers within the range of the board
-main = do  
+main = do
 
           gen1 <- getStdGen
           putStrLn $ show $ createMove $ take 2 $ (randomRs ('0','4') gen1)
 
+createCoordBoard :: Board -> [[(Int, Int)]] -> [[((Int, Int), Cell)]]
+createCoordBoard [] [] = [[(())]]
+createCoordBoard (x:xs) (y:ys) = zip x : createCoordBoard xs ys
 
+
+pickPiece :: Board -> Player -> (Int, Int)
+pickPiece (x:xs) Black =
 
 -- Creates the final move to be returned
 -- So far, only takes the randomly generated move within the board and creates a tuple
@@ -33,14 +46,12 @@ createMove (x:xs) = zip [digitToInt x] [digitToInt $ head xs]
 
 
 -- Checks the GameState and chooses a random piece in play to move
-pickPiece :: [(Integer, Integer)]
-pickPiece = [(0,0)]
+randomAI :: Chooser
+randomAI b Normal        c = return (Just [(0,0),(1,2)])
+randomAI b PawnPlacement c = return (Just [(2,2)])
 
 
 -- Check GameState for pieces for that player
 -- Chooses a random piece to move from the GameState
 --checkGameState :: GameState -> Player -> (Int, Int)
 --checkGameState GameState Player = (1,0)
-  
-
-
