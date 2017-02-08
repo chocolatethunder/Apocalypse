af8b53bd33866c867ae24892121cb7f08c69b4ca
kstelter <kaylee.stelter@ucalgary.ca>
2017-02-07 23:18:59 -0700

    Updates to RandomAI
    Now generates list of pieces based on the colour of the player. Need to
    implement a random choice from the generated list and then generate a
    random move based on the selected piece.

--- ApocAIRandom.hs
+++ ApocAIRandom.hs
@@ -4,13 +4,13 @@
 
 
 
-module ApocAIRandom
-       (main,
-       createMove) where
+--module ApocAIRandom
+--       (main) where
 
 import System.Random
 import Data.Char
 import ApocTools
+
 
 -- List of coordinates representing the game board
 coordinateBoard =  [ [(0,0), (0,1), (0,2), (0,3), (0,4)],
@@ -19,39 +19,57 @@ coordinateBoard =  [ [(0,0), (0,1), (0,2), (0,3), (0,4)],
                     [(3,0), (3,1) , (3,2) , (3,3) , (3,4)],
                     [(4,0), (4,1), (4,2), (4,3), (4,4)] ]
 
+-- Sample game board for testing -- to be removed for final version
+gameBoard =           [ [WK, WP, WP, WP, WK],
+                      [WP, E , E , E , WP],
+                      [E , E , E , E , E ],
+                      [BP, E , E , E , BP],
+                      [BK, BP, BP, BP, BK] ]
 
--- Randomly chooses from a legal move, pass, or goof
-
--- Should also handle the pawn placement
-
--- Generates two integers within the range of the board
+-- Creates lists based on the current player and chooses a piece to move
 main = do
+         let player = White -- to be removed for final version
+         let coordList = concat $ createCoordList coordinateBoard gameBoard
 
-          gen1 <- getStdGen
-          putStrLn $ show $ createMove $ take 2 $ (randomRs ('0','4') gen1)
+         if player == White then do
+                                    let wkList = filter ((==WK).snd) coordList
+                                    let wpList = filter ((==WP).snd) coordList
+                                    let pieceList = wkList ++ wpList
+                                    putStrLn $ show $ pieceList
+                                    return ()
 
-createCoordBoard :: Board -> [[(Int, Int)]] -> [[((Int, Int), Cell)]]
-createCoordBoard [] [] = [[(())]]
-createCoordBoard (x:xs) (y:ys) = zip x : createCoordBoard xs ys
+         else do
+                                    let bkList = filter ((==BK).snd) coordList
+                                    let bpList = filter ((==BP).snd) coordList
+                                    let pieceList = bkList ++ bpList
+                                    putStrLn $ show $ pieceList
+                                    return ()
+
+-- Creates a list of coordinate-piece pairs
+createCoordList :: [[(Int, Int)]] -> [[Cell]] -> [[((Int, Int), Cell)]]
+createCoordList _ [] = []
+createCoordList [] _ = []
+createCoordList (x:xs) (y:ys) = zip x y : createCoordList xs ys
+
+-- Implement function which takes in the generated piece list and returns a randomly chosen piece to move
+
+-- Implement function which generates a random move for the selected type of piece
+
+-- Implement function which handles pawn PawnPlacement
+
+-- Implement a function which randomly chooses between a random move or pass
 
 
-pickPiece :: Board -> Player -> (Int, Int)
-pickPiece (x:xs) Black =
-
--- Creates the final move to be returned
--- So far, only takes the randomly generated move within the board and creates a tuple
--- Needs to incorporate the randomly chosen piece from the GameState
-createMove :: String -> [(Int, Int)]
-createMove (x:xs) = zip [digitToInt x] [digitToInt $ head xs]
 
 
--- Checks the GameState and chooses a random piece in play to move
-randomAI :: Chooser
-randomAI b Normal        c = return (Just [(0,0),(1,2)])
-randomAI b PawnPlacement c = return (Just [(2,2)])
+--- Leftover Functions
 
+          -- Checks the GameState and chooses a random piece in play to move
+          --randomAI :: Chooser
+          --randomAI b Normal        c = return (Just [(0,0),(1,2)])
+          --randomAI b PawnPlacement c = return (Just [(2,2)])
 
--- Check GameState for pieces for that player
--- Chooses a random piece to move from the GameState
---checkGameState :: GameState -> Player -> (Int, Int)
---checkGameState GameState Player = (1,0)
+          --gen1 <- getStdGen
+          --print gameBoard
+
+          --putStrLn $ show $ createMove $ take 2 $ (randomRs ('0','4') gen1)
