9ab998f128be6ca9e98b801931a477be81683492
Kowther Hassan <kowtherh@gmail.com>
2017-02-10 17:42:21 -0700

    Random AI Nearly Complete
    RandomAI should now output valid legal moves, pass when there are no
    valid legal moves, and output a valid pawn placement if requested. Code
    clean up and commenting to be completed this weekend.

--- AI/Random.hs
+++ AI/Random.hs
@@ -1,9 +1,8 @@
 -- Random AI code
 -- TO DO --
--- Pawn can ONLY move forward to an empty square, no enemy piece can be there
 -- Handle pawn PawnPlacement
 -- Handle if there are no legal moves
--- Fix diagonal move choice
+
 
 module AI.Random where
 
@@ -29,15 +28,43 @@ aiRandom gameState Normal player =
          let pieceList = generatePieceList coordList player
          let possibleMoves = filterPossible pieceList player
          let possibleMovesChar = createMoveCharList possibleMoves (theBoard gameState)
-         let legalMoves = filterLegal (createCoordList possibleMoves possibleMovesChar) player
-         let lengthList = (length legalMoves - 1)
-         randomNum <- generateRandom lengthList
-         let finalPiece = pickElem pieceList randomNum
-         let moveElem = pickElem legalMoves randomNum
-         let lengthMoveElem = (length moveElem - 1)
-         randomNum2 <- generateRandom lengthMoveElem
-         let finalMove = pickElem moveElem randomNum2
-         return (Just [(fst finalPiece), (fst finalMove)])
+         let legalMoves = removeBadPawnMoves pieceList (filterLegal (createCoordList possibleMoves possibleMovesChar) player)
+         let cleanedPieceList = removeEmptyPieceList pieceList legalMoves
+         let cleanedLegalMoves = removeEmptyLegalMoveList legalMoves
+         if (checkPass cleanedPieceList ) then return Nothing
+           else do
+                   let lengthList = (length cleanedLegalMoves - 1)
+                   randomNum <- generateRandom lengthList
+                   let finalPiece = pickElem cleanedPieceList randomNum
+                   let moveElem = pickElem cleanedLegalMoves randomNum
+                   let lengthMoveElem = (length moveElem - 1)
+                   randomNum2 <- generateRandom lengthMoveElem
+                   let finalMove = pickElem moveElem randomNum2
+                   return (Just [(fst finalPiece), (fst finalMove)])
+aiRandom gameState PawnPlacement player =
+    do
+        let coordList = concat $ createCoordList coordinateBoard (theBoard gameState)
+        let legalMoves = filterEmpty coordList
+        randomNum <- generateRandom (length legalMoves -1)
+        let finalMove = pickElem legalMoves randomNum
+        return (Just [(fst finalMove)])
+
+
+
+removeEmptyPieceList :: [((Int, Int), Cell)] -> [[((Int, Int), Cell)]] -> [((Int, Int), Cell)]
+removeEmptyPieceList [] [] = []
+removeEmptyPieceList (x:xs) (y:ys) = if ( y == [] ) then removeEmptyPieceList xs ys else x: removeEmptyPieceList xs ys
+
+removeEmptyLegalMoveList ::  [[((Int, Int), Cell)]] -> [[((Int, Int), Cell)]]
+removeEmptyLegalMoveList  y = filter (/=[]) y
+
+
+checkPass :: [a] -> Bool
+checkPass list
+              | (length list == 0 ) = True
+              | otherwise = False
+
+
 
 -- Creates a list of pieces for that player and their coordinates
 generatePieceList :: [((Int, Int), Cell)] -> Player -> [((Int, Int), Cell)]
@@ -53,14 +80,28 @@ generatePieceList coordList player =
                            kList ++ pList
 
 removeBadPawnMoves :: [((Int, Int), Cell)] -> [[((Int, Int), Cell)]] -> [[((Int, Int), Cell)]]
+removeBadPawnMoves [] [] = []
 removeBadPawnMoves (x:xs) (y:ys) =
-                                    if ((snd x) == WK (||) BK)
-                                        then y : removeBadPawnMoves xs ys
-                                    else do
-                                        if
+                                   if (((snd x) == WK) ||  ((snd x) == BK))
+                                       then y : removeBadPawnMoves xs ys
+                                   else do
+                                           if (isDiagonal x y False) then (removeBadDiagonalMoves x y  : removeBadPawnMoves xs ys)
 
-                                        else
-                                        (filter ((==E).snd) y) : removeBadPawnMoves xs ys
+                                           else (filter ((==E).snd) y) : removeBadPawnMoves xs ys
+
+-- Checks where the a list of possible moves for a coordinate has a diagonal move
+isDiagonal :: ((Int, Int), Cell) -> [((Int, Int), Cell)] -> Bool -> Bool
+isDiagonal a [] True = True
+isDiagonal a [] False = False
+isDiagonal a (y:ys) bool = if ((fst $ fst a)  /= (fst $ fst y))  then isDiagonal a ys True
+                          else if ( bool == True ) then isDiagonal a ys True else isDiagonal a ys False
+
+
+removeBadDiagonalMoves :: ((Int, Int), Cell) -> [((Int, Int), Cell)] -> [((Int,Int),Cell)]
+removeBadDiagonalMoves a [] = []
+removeBadDiagonalMoves a (x:xs) = if ((fst $ fst a)  /= (fst $ fst x) && (snd x /= E) )
+                                 then x : removeBadDiagonalMoves a xs
+                                 else  if (fst $ fst a)  == (fst $ fst x) then x : removeBadDiagonalMoves a xs else removeBadDiagonalMoves a xs
 
 
 -- Creates a list of coordinate-piece pairs
@@ -73,7 +114,7 @@ createCoordList (x:xs) (y:ys) = zip x y : createCoordList xs ys
 filterPossible :: [((Int, Int), Cell)] -> Player -> [[(Int, Int)]]
 filterPossible [] player = []
 filterPossible (x:xs) player = if ((snd x) == WP) || ((snd x) == BP) then
-                                                    (legalPawnMoves (fst x) White True) : filterPossible xs player
+                                                    (legalPawnMoves (fst x) player True) : filterPossible xs player
                                             else
                                                     (legalKnightMoves (fst x)) : filterPossible xs player
 
@@ -101,3 +142,8 @@ generateRandom lengthList = randomRIO (0, lengthList)
 -- Given a randomly generated index, returns the element at that index
 pickElem :: [a] -> Int -> a
 pickElem list index = list !! index
+
+-- For PawnPlacement, will remove all moves that are not empty
+filterEmpty :: [((Int, Int), Cell)] -> [((Int, Int), Cell)]
+filterEmpty [] = []
+filterEmpty  (x:xs) = if (snd x == E) then x: filterEmpty xs else filterEmpty xs
--- Lib/RunGame.hs
+++ Lib/RunGame.hs
@@ -1,8 +1,8 @@
--- This module is just a giant loop. This is the meat of the whole thing. 
+-- This module is just a giant loop. This is the meat of the whole thing.
 -- It runs the game until the endgame scenario is reach. It check and categorizes
--- each move made by the user/computer then updates the game state. 
+-- each move made by the user/computer then updates the game state.
 
-module Lib.RunGame (gameLoop) where 
+module Lib.RunGame (gameLoop) where
 
 import Data.Maybe
 import Control.Monad
@@ -16,74 +16,74 @@ import AI.Random
 
 
 -- Use game loop with the current game state and whether we
--- have determined whether the game should end or not. All gl_ functions 
--- are relevent to the gameloop flow. 
+-- have determined whether the game should end or not. All gl_ functions
+-- are relevent to the gameloop flow.
 gameLoop :: GameState -> [Char] -> [Char] -> PlayType -> Bool -> IO()
-gameLoop currBoard bl wt playType endGame = do 
-                                if (endGame == False) then                                    
-                                    do 
-                                        
+gameLoop currBoard bl wt playType endGame = do
+                                if (endGame == False) then
+                                    do
+
                                         {- --------- AREA IS UNDER CONSTRUCTION ------------- -}
-                                        
+
                                         -- check if a player has run out of pawns
                                         let blackPawnsLeft = arePawnsLeft (theBoard currBoard) Black
                                         let whitePawnsLeft = arePawnsLeft (theBoard currBoard) White
-                                        
+
                                         -- store the current penalty points
                                         let blackPenalty = (blackPen currBoard)
                                         let whitePenalty = (whitePen currBoard)
-                                        
+
                                          -- END GAME CHECK
                                             -- End game conditions:
-                                            --      One of the players looses all his/her pawns.  The other player is the winner. 
+                                            --      One of the players looses all his/her pawns.  The other player is the winner.
                                             --      One of the players accumulates two penalty points.  The other player is the winner.
                                         if (blackPawnsLeft == False || whitePawnsLeft == False || blackPenalty >= 2 || whitePenalty >= 2) then
                                             gameLoop currBoard bl wt Normal True
                                         else
-                                            
-                                            do 
+
+                                            do
                                                 -- get the data from the command line
                                                 -- incoming data is a Maybe [(Int,Int)]
                                                 blackMove <- (getPlayerMove currBoard bl playType Black) -- Raw incoming data
                                                 whiteMove <- (getPlayerMove currBoard wt playType White) -- Raw incoming data
-                                                
-                                                -- send it to be parsed then pushed into 
+
+                                                -- send it to be parsed then pushed into
                                                 -- the current game state. Need to pass p1 p2
-                                                -- so that we know what type of players are 
+                                                -- so that we know what type of players are
                                                 -- playing when looping back to the beginning
-                                                
-                                                -- putStrLn (show(getFromBoard (theBoard currBoard) ((fromJust p1move) !! 0)))              
-                                                
+
+                                                -- putStrLn (show(getFromBoard (theBoard currBoard) ((fromJust p1move) !! 0)))
+
                                                 --let blackPlay = case blackMove of Nothing -> Passed :: Played
                                                 --                                  xs -> Played ((fromJust xs)!!0,(fromJust xs)!!1)
                                                 --let whitePlay = case whiteMove of Nothing -> Passed :: Played
                                                 --                                  xs -> Played ((fromJust xs)!!0,(fromJust xs)!!1)
-                                                
+
 
                                                 -- Determine if it is a Nothing, Length 1 List, Length 2 List
                                                 -- if Nothing then set played = Passed
-                                                -- 
+                                                --
                                                 -- if Length 1 then check if the player is trying to upgrade to knight
-                                                    -- get the piece at the coordinate 
-                                                    -- if 
+                                                    -- get the piece at the coordinate
+                                                    -- if
                                                         -- it is a pawn
-                                                        -- it is at (y=4(black) or y=0(white)) 
+                                                        -- it is at (y=4(black) or y=0(white))
                                                         -- numKnights is <2 then upgrade
                                                         -- dest is empty
                                                         -- then upgrade pawn
-                                                    -- 
-                                                
+                                                    --
+
                                                 --      Both players pass on the same round. The one with the most pawns wins.
-                                                if ((blackMove == Nothing && whiteMove == Nothing)) then   
-                                                    do 
+                                                if ((blackMove == Nothing && whiteMove == Nothing)) then
+                                                    do
                                                         let newBoard = GameState (Passed)
                                                                                   (blackPenalty)
                                                                                   (Passed)
                                                                                   (whitePenalty)
                                                                                   (theBoard currBoard)
-                                                        gameLoop newBoard bl wt Normal True                                            
-                                                else                                         
-                                                    -- BEGIN OTHER CHECKS 
+                                                        gameLoop newBoard bl wt Normal True
+                                                else
+                                                    -- BEGIN OTHER CHECKS
                                                     do
                                                         isBlackMoveValid <- validateMove currBoard blackMove Black
                                                         isWhiteMoveValid <- validateMove currBoard whiteMove White
@@ -92,28 +92,28 @@ gameLoop currBoard bl wt playType endGame = do
                                                                                                        True -> blackPenalty
                                                         let blackPlayed = case isBlackMoveValid of False -> Goofed (getTwoCoords blackMove)
                                                                                                    True -> Played (getTwoCoords blackMove)
-                                                        
+
 
 
                                                         let whitePlayed = case isWhiteMoveValid of False -> Goofed (getTwoCoords whiteMove)
                                                                                                    True -> Played (getTwoCoords whiteMove)
                                                         let newWhitePenalty = case isWhiteMoveValid of False -> succ whitePenalty
                                                                                                        True -> whitePenalty
-                                                        
+
 
                                                         -- DEBUG
                                                         {-
                                                         putStrLn "\n---- DEBUG ----"
-                                                        
+
                                                         putStrLn (show (newBlackPenalty))
                                                         putStrLn (show (newWhitePenalty))
-                                                        
+
                                                         putStrLn (show (isBlackMoveValid))
                                                         putStrLn (show (isWhiteMoveValid))
-                                                        
+
                                                         putStrLn (show (getPawnsLeft (theBoard currBoard) Black))
                                                         putStrLn (show (getPawnsLeft (theBoard currBoard) White))
-                                                        
+
                                                         putStrLn "---- DEBUG ----\n"
                                                         -}
 
@@ -122,7 +122,7 @@ gameLoop currBoard bl wt playType endGame = do
                                                         let updateBoard' = replace2 (updateBoard) (fst(getTwoCoords whiteMove)) E
                                                         let updateBoard'' = replace2 (updateBoard') (snd(getTwoCoords blackMove)) (getFromBoard (theBoard currBoard) (fst(getTwoCoords blackMove)))
                                                         let updateBoard''' = replace2 (updateBoard'') (fst(getTwoCoords blackMove)) E
-                                                        
+
                                                         -- Save game state here
                                                         let newBoard = GameState (blackPlayed)
                                                                                   (newBlackPenalty)
@@ -131,23 +131,23 @@ gameLoop currBoard bl wt playType endGame = do
                                                                                   (updateBoard''')
                                                         -- Loop back
                                                         putStrLn (show (newBoard))
-                                                        gameLoop newBoard bl wt Normal False 
+                                                        gameLoop newBoard bl wt Normal False
 
-                                                        
+
                                                 {- --------- CONSTRUCTION ENDS ------------- -}
-                                        
+
                                 else
-                                    do 
+                                    do
                                         -- this does NOT account for a tie yet
-                                        if (blackPen currBoard >= 2 || arePawnsLeft (theBoard currBoard) Black) then 
+                                        if (blackPen currBoard >= 2 || arePawnsLeft (theBoard currBoard) Black) then
                                             endGameScene White
-                                        else 
+                                        else
                                             endGameScene Black
                                 return ()
 
 
 
- 
+
 -- this determines what type of playertype is playing
 -- WARNING!! This is NOT dynamic. If the strats change
 -- please update this function accordingly.
@@ -163,10 +163,10 @@ getPlayerMove currBoard stratType playType playerType
     | otherwise = return (Just [(0,0)]) -- this needs to error out. To do
 
 -- This function will pass the game board to the
--- appropriate AI type and get a Maybe [(Int,Int)] of 
+-- appropriate AI type and get a Maybe [(Int,Int)] of
 -- correctly formatted coordinates. (Work in progress)
 aiMove :: GameState -> PlayType -> Player -> [Char] -> IO (Maybe [(Int,Int)])
-aiMove currBoard playType playerType aiType 
+aiMove currBoard playType playerType aiType
     | aiType == "offensive" = aiOffensive currBoard playType playerType
     | aiType == "random" = aiRandom currBoard playType playerType
     | otherwise = return(Just [(0,0)]) -- this needs to error out. To do
